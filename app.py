78540f09e38647dcffba4e5944a4c97bd1392c79
PeterGamelli <petergamelli2277@gmail.com>
2025-08-02 19:19:02 -0400

    Login added to index

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Flask instance
 app = Flask(__name__)
-
+             
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template("user.html", user_name=name)
-
-@app.route('/register', methods=['GET', 'POST'])
-def registration():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
-        return f"Registered: {username} with password {password}"
-    return render_template("register.html")
-
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
